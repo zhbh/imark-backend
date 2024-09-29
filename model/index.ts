538da15a662e52ef082277107d5b14553f287881
Adam Zh. <zh.bh@qq.com>
2024-09-30 05:43:39 +1300

    update

--- model/index.ts
+++ model/index.ts
@@ -4,11 +4,11 @@ import userSchema from "./userModel";
 import categorySchema from "./categoryModel";
 import favoriteSchema from "./favoriteModel";
 
-import dotenv from "dotenv"; 
-dotenv.config();
+import dotenvx from "@dotenvx/dotenvx"; 
+dotenvx.config();
 
 async function main() {
-    await mongoose.connect( "mongodb://test:test@ac-agmggzi-shard-00-00.6cniiol.mongodb.net:27017,ac-agmggzi-shard-00-01.6cniiol.mongodb.net:27017,ac-agmggzi-shard-00-02.6cniiol.mongodb.net:27017/?ssl=true&replicaSet=atlas-llj3gt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=imark-cluster" || "");
+    await mongoose.connect( process.env.MONGODB_URI || "");
 }
 
 main()
