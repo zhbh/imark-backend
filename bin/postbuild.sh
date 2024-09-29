5950303d1a3aeb2b144f4e765d9e43def6d1b10e
Adam Zh. <zh.bh@qq.com>
2024-09-30 06:29:33 +1300

    Update postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,6 +4,9 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
+touch ./dist/.env
+echo "MONGODB_URI=${MONGODB_URI}" >> ./dist/.env
+
 cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
